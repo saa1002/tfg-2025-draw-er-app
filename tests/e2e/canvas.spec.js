1503cad1ef33a95e40fc669462321462688e5cf8
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2024-05-18 19:45:03 +0200

    test: add attributes tests

---
+++ tests/e2e/canvas.spec.js
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test('add entities to the canvas and change name', async ({ page }) => {
+    await page.goto('/');
+
+    const canvas = page.locator("svg")
+    await page.getByRole('img').first().dragTo(canvas);
+
+    await canvas.click();
+
+    await page.getByText('Entidad').first().dblclick();
+    await page.keyboard.type('Clientes');
+
+    await canvas.click();
+});
+
+test('add attributes to an entity', async ({ page }) => {
+    await page.goto('/');
+
+    const canvas = page.locator("svg")
+    await page.getByRole('img').first().dragTo(canvas);
+
+    await canvas.click();
+
+
+    // Añadir un atributo primario
+    await page.getByText('Entidad').first().click();
+    await page.getByText('Añadir atributo').first().click();
+    await page.getByText('Añadir atributo primario').first().click();
+
+    await page.getByText('Atributo', {exact: true}).first().dblclick();
+    await page.keyboard.type('Atributo primario');
+    await canvas.click();
+
+    // Añadir un atributo secundario
+    await page.getByText('Entidad').first().click();
+    await page.getByText('Añadir atributo').first().click();
+    await page.getByText('Añadir atributo', {exact: true}).first().click();
+});
